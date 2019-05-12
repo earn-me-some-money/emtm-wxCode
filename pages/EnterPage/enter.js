24055cf783974ecedbd9af3cd78f90702bddf3dd
Cait7 <552378875@qq.com>
2019-05-12 23:10:09 +0800

    Initial Commit

---
+++ pages/EnterPage/enter.js
@@ -0,0 +1,12 @@
+// pages/EnterPage/enter.js
+//test.js  
+//获取应用实例  
+var app = getApp()
+Page({
+  data: {
+    userInfo: {}
+  },
+  onLoad: function () {
+    console.log('onLoad test');
+  }
+})  
