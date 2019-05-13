b82f8f0ba8cef134c12f8b83e193c4400cce89bd
Cait7 <552378875@qq.com>
2019-05-14 02:08:26 +0800

    mainPage

--- pages/EnterPage/enter.js
+++ pages/EnterPage/enter.js
@@ -1,10 +1,9 @@
 // pages/EnterPage/enter.js
 //test.js  
 //获取应用实例  
-var app = getApp()
 Page({
   data: {
-    userInfo: {}
+    picList: ["http://img1.imgtn.bdimg.com/it/u=1739733360,1788222103&fm=26&gp=0.jpg", "http://img1.gtimg.com/cul/pics/hv1/148/6/2187/142211353.jpg", "http://img008.hc360.cn/k3/M04/CE/F9/wKhQv1gEwvqEH6QAAAAAALn_tIw839.jpg"]
   },
   onLoad: function () {
     console.log('onLoad test');
